eb9657dd9489a63abdfa618a7076f4a32ed74362
RohithR24 <srohithreddy.edu@gmail.com>
2024-09-24 10:43:50 -0700

    Adding all the props

---
+++ public/images/index.js
@@ -0,0 +1,14 @@
+
+import hero from './Hero.jpg';
+import campus1 from './campus1.jpg';
+import campus2 from './campus2.jpg';
+// import thumbnailBackground from './thumbnail-background.svg'
+// import thumbnailShoe1 from './thumbnail-shoe1.svg'
+// import thumbnailShoe2 from './thumbnail-shoe2.svg'
+// import thumbnailShoe3 from './thumbnail-shoe3.svg'
+
+export {
+    hero,
+    campus1,
+    campus2
+}
