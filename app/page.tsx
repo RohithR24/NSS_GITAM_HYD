b7a0029d9504b1cb01573737836f584b53151a64
RohithR24 <srohithreddy.edu@gmail.com>
2024-09-25 15:05:09 -0700

    Adding statistics

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Hero from "@/components/sections/hero";
+import Statistics from "@/components/sections/statistics";
 export default function Home() {
   return (
-    <div className="flex items-center justify-items-center min-h-screen pb-20 gap-16 lg:p-0 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Hero />
+    <div className="flex flex-col items-center justify-items-center min-h-screen gap-16 lg:p-0 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <Hero /> 
+      <section className='padding-b' > <Statistics /> </section>
     </div>
   );
 }
---
+++ components/sections/statistics.tsx
@@ -0,0 +1,87 @@
+import React from "react";
+import {
+  blood,
+  bookdonation,
+  bookdonation2,
+  volunteer,
+  collaboration
+} from "../../public/icons/index";
+import Image from "next/image";
+const Statistics = () => {
+  return (
+    <section className="flex flex-col">
+      <p className="font-semibold text-2xl md:text-3xl text-center">
+        Our Outstanding Impact
+      </p>
+      <div className="mt-10 grid grid-cols-2 lg:grid-cols-4 gap-y-5 lg:gap-y-0 gap-x-5 place-items-center w-full mx-auto max-w-7xl px-5">
+        <div className="flex flex-col justify-center items-center bg-[#FFF6F3] px-4 h-[126px] w-[100%] md:w-[281px] md:h-[192px] rounded-lg justify-self-center">
+          <div className="flex flex-row justify-center items-center">
+            <Image
+              src={volunteer}
+              alt="volunteer icon"
+              width={45}
+              height={45}
+            />
+            <p className="font-bold text-3xl sm:text-4xl lg:text-5xl leading-9 text-primary ml-2">
+              500+
+            </p>
+          </div>
+          <p className="font-medium text-base sm:text-lg leading-6 mt-3 md:mt-6 text-center">
+            Active Volunteers
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-[#FFF6F3] px-4 h-[126px] w-[100%] md:w-[281px] md:h-[192px] rounded-lg justify-self-center">
+          <div className="flex flex-row justify-center items-center">
+            <Image
+              src={blood}
+              alt="Blood Donation icon"
+              width={45}
+              height={45}
+            />
+            <p className="font-bold text-3xl sm:text-4xl lg:text-5xl leading-9 text-primary ml-2">
+              20+
+            </p>
+          </div>
+          <p className="font-medium text-base sm:text-lg leading-6 mt-3 md:mt-6 text-center">
+            Blood Donation Camps
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-[#FFF6F3] px-4 h-[126px] w-[100%] md:w-[281px] md:h-[192px] rounded-lg justify-self-center">
+          <div className="flex flex-row justify-center items-center">
+            <Image
+              src={bookdonation}
+              alt="Book Donation icon"
+              width={45}
+              height={45}
+            />
+            <p className="font-bold text-3xl sm:text-4xl lg:text-5xl leading-9 text-primary ml-2">
+              30+
+            </p>
+          </div>
+          <p className="font-medium text-base sm:text-lg leading-6 mt-3 md:mt-6 text-center">
+            Book Distributions
+          </p>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-[#FFF6F3] px-4 h-[126px] w-[100%] md:w-[281px] md:h-[192px] rounded-lg justify-self-center">
+          <div className="flex flex-row justify-center items-center">
+            <Image
+              src={collaboration}
+              alt="collaboration icon"
+              width={45}
+              height={45}
+            />
+
+            <p className="font-bold text-3xl sm:text-4xl lg:text-5xl leading-9 text-primary ml-2">
+              10+
+            </p>
+          </div>
+          <p className="font-medium text-base sm:text-lg leading-6 mt-3 md:mt-6 text-center">
+            Active Collaborations
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Statistics;
