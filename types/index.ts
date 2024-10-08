801fe5adc7f2b201b6850f3b92e09354470581fa
RohithR24 <srohithreddy.edu@gmail.com>
2024-10-08 00:00:33 -0700

    Adding interfaces

--- types/index.ts
+++ types/index.ts
@@ -1,15 +1,13 @@
-export interface CardProps{
-    image: string;
-    title: string;
-    content: string;
-    
+export interface CardProps {
+  image: string;
+  title: string;
+  content: string;
 }
 
-export interface ButtonProps{
-    title: string;
-    color: string;
+export interface ButtonProps {
+  title: string;
+  color: string;
 }
-
 
 export interface StatCardProps {
   icon: string | any;
@@ -18,24 +16,59 @@ export interface StatCardProps {
 }
 
 export interface TeamMemberProps {
-  id: number,           
-  name: string,
-  role: string,
-  image: any,
+  id: number;
+  name: string;
+  role: string;
+  image: any;
   social: {
-    linkedin?: string,
-    instagram?: string,
-    facebook?: string,
-  }
+    linkedin?: string;
+    instagram?: string;
+    facebook?: string;
+  };
 }
 
 export interface TeamProfileProps {
-  year: string,
-  head: TeamMemberProps,
-  faculty: TeamMemberProps[],  
-  students: TeamMemberProps[]  
+  year: string;
+  head: TeamMemberProps;
+  faculty: TeamMemberProps[];
+  students: TeamMemberProps[];
 }
 
-export interface TeamDataProps{
-  teamData: TeamProfileProps
+export interface TeamDataProps {
+  teamData: TeamProfileProps;
+}
+
+export interface TeamMember {
+  id: number;
+  name: string;
+  role: string;
+  image: string | any;
+  social: {
+    linkedin?: string;
+    instagram?: string;
+    facebook?: string;
+  };
+}
+
+export interface Team {
+  id: number;
+  name: string;
+  year: number;
+  description: string;
+}
+
+export interface TabsProps {
+  focusAreas: string[];
+  activeTab: string;
+  onTabSelect: (area: string) => void;
+}
+
+export interface InitiativeCardProps {
+  image: string;
+  caption: string;
+}
+export interface InitiativeProps {
+  id: string;
+  name: string;
+  initiatives: InitiativeCardProps[];
 }
