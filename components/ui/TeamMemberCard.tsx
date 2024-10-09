e36c4caf75e04e939f1afc643b480f7031b13a6a
RohithR24 <srohithreddy.edu@gmail.com>
2024-10-08 23:31:31 -0700

    Feat: adding the team animation

--- components/ui/TeamMemberCard.tsx
+++ components/ui/TeamMemberCard.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 const SocialLinks: React.FC<{ social: TeamMember["social"] }> = ({
   social,
 }) => (
-  <div className="flex items-center justify-center space-x-2 bg-color-white">
+  <div className="flex items-center justify-center space-x-2 bg-color-white py-1">
     {social.linkedin && (
       <a
         href={social.linkedin}
@@ -46,110 +46,53 @@ export const TeamMemberCard: React.FC<{
   member: TeamMember;
   isHead?: boolean;
   activeTab: string;
-}> = ({ member, isHead = false, activeTab }) => {
+  index: number;
+}> = ({ member, isHead, activeTab, index }) => {
   const [isHovered, setIsHovered] = useState(false);
-
   return (
-    <div>
-      {isHead && (
-        <div className="flex flex-col items-center mb-12">
-          <div className="relative w-40 h-40 rounded-full overflow-hidden mb-4">
-            <Image
-              src={member.image}
-              alt={member.name}
-              layout="fill"
-              objectFit="cover"
-            />
-          </div>
-          <h2 className="text-2xl font-semibold">{member.name}</h2>
-          <p className="text-gray-600">{member.role}</p>
-        </div>
-      )}
-
-      <div className="flex flex-col justify-center items-center">
-      {(!isHead) && (
-        <motion.div
-          initial={{ opacity: 0, y: 20 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 0.5 }}
-          onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
-          className={`grid gap-8 ${
-            activeTab === "faculty"
-              ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
-              : "grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"
-          }`}
-        >
-          <motion.div
-            whileHover={{ scale: 1.05 }}
-            className="flex flex-col items-center"
-          >
-            <div
-              className={`relative rounded-full overflow-hidden mb-4 ${
-                activeTab === "faculty" ? "w-32 h-32" : "w-24 h-24"
-              }`}
-            >
-              <Image
-                src={member.image}
-                alt={member.name}
-                layout="fill"
-                objectFit="cover"
-              />
-            </div>
-            <h3
-              className={`font-semibold text-center ${
-                activeTab === "faculty" ? "text-lg" : "text-base"
-              }`}
-            >
-              {member.name}
-            </h3>
-            <p className="text-gray-600 text-center text-sm">{member.role}</p>
-          </motion.div>
-        </motion.div>
-      )}
+    <motion.div
+      key={index}
+      className="flex flex-col items-center"
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      whileHover={{ scale: 1.10 }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      transition={{ duration: 0.3, delay: index * 0.1 }}
+    >
+      <div className="relative w-32 h-32 mb-4">
+        <Image
+          src={member.image}
+          alt={member.name}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-full"
+        />
       </div>
-      
-      {/* <motion.div
-        className={`grid gap-8 ${
-          activeTab === "faculty"
-            ? "grid-cols-1 sm:grid-cols-2 lg:grid-cols-3"
-            : "grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5"
-        }`}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
+
+      <h3
+        className={`font-semibold ${
+          isHead ? "text-xl" : "text-lg "
+        } text-center`}
       >
-        <div className="relative mb-2">
-          <Image
-            src={member.image}
-            alt={member.name}
-            width={isHead ? 200 : 100}
-            height={isHead ? 200 : 100}
-            className="rounded-full border-4 border-primary-500 shadow-lg"
-          />
-        </div>
-        <h3 className="text-lg font-semibold text-gray-800">{member.name}</h3>
-        <div className="flex justify-center items-center my-2">
-          <AnimatePresence>
-            {isHovered ? (
-              <motion.div
-                initial={{ opacity: 0, y: -10 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: -10 }}
-                transition={{ duration: 0.2 }}
-                className=" bottom-full left-1/2 transform -translate-x-1/2"
-              >
-                <SocialLinks social={member.social} />
-              </motion.div>
-            ) : (
-              <p className="text-sm text-gray-600">{member.role}</p>
-            )}
-          </AnimatePresence>
-        </div>
-      </motion.div> */}
-    </div>
+        {member.name}
+      </h3>
+      <AnimatePresence mode="wait">
+        {isHovered ? (
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.3, delay: 0.3 }}
+            className=" bottom-full left-1/2 transform -translate-x-1/2"
+          >
+            <SocialLinks social={member.social} />
+          </motion.div>
+        ) : (
+          <p className="text-sm text-gray-600 py-1">{member.role}</p>
+        )}
+      </AnimatePresence>
+    </motion.div>
   );
 };
 
--- components/ui/TeamStructure.tsx
+++ components/ui/TeamStructure.tsx
@@ -4,12 +4,14 @@ import React, { useState } from "react";
 import { TeamDataProps } from "@/types";
 import { Tabs, TeamMemberCard } from "@/components/ui/index";
 import { teamTabTitles } from "@/constants";
+import { AnimatePresence, motion } from "framer-motion";
 
 export default function TeamStructure({ teamData }: TeamDataProps) {
   const [activeTab, setActiveTab] = useState<string>(teamTabTitles[0].id);
 
   // Dynamically compute the selected team based on active tab
-  const selectedTeam = activeTab === "student" ? teamData.students : teamData.faculty;
+  const selectedTeam =
+    activeTab === "student" ? teamData.students : teamData.faculty;
 
   const handleTabSelect = (area: string) => {
     setActiveTab(area);
@@ -18,29 +20,55 @@ export default function TeamStructure({ teamData }: TeamDataProps) {
   return (
     <div>
       {/* Tabs Component */}
-      <Tabs title={teamTabTitles} activeTab={activeTab} onTabSelect={handleTabSelect} />
+      <Tabs
+        title={teamTabTitles}
+        activeTab={activeTab}
+        onTabSelect={handleTabSelect}
+      />
 
       {/* Team Structure Section */}
-      <section className="bg-gradient-to-br from-primary-50 to-primary-100 py-16">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex flex-col items-center">
-            {/* Head Member Card */}
-            {activeTab === "faculty" && (
-              <div className="flex flex-col items-center justify-center">
-                <TeamMemberCard member={teamData.head} isHead activeTab={activeTab}/>
-                <div className="w-px h-16 bg-gray-300 my-8"></div>
-              </div>
-            )}
- 
-            {/* Selected Team Members */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-12">
-              {selectedTeam.map((member) => (
-                <TeamMemberCard key={member.id} member={member} activeTab = {activeTab}/>
-              ))}
+      <div className=" mx-auto px-4 sm:px-6 lg:px-8">
+          {/* Head Member Card */}
+          {activeTab === "faculty" && (
+            <div className="flex flex-col items-center justify-center">
+              <TeamMemberCard
+                member={teamData.head}
+                isHead
+                activeTab={activeTab}
+                index={0}
+              />
+              <div className="w-px h-16 bg-gray-300 my-8"></div>
             </div>
-          </div>
-        </div>
-      </section>
+          )}
+
+          {/* Selected Team Members */}
+          <AnimatePresence mode="wait">
+            <motion.div
+              key={activeTab}
+              initial={{ opacity: 0, x: 20 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -20 }}
+              transition={{ duration: 0.3 }}
+            >
+              <div
+                className={`grid gap-8 ${
+                  selectedTeam.length <= 6
+                    ? "md:grid-cols-4"
+                    : "md:grid-cols-4 lg:grid-cols-4"
+                }`}
+              >
+                {selectedTeam.map((member, index) => (
+                  <TeamMemberCard
+                    key={member.id}
+                    member={member}
+                    activeTab={activeTab}
+                    index={index}
+                  />
+                ))}
+              </div>
+            </motion.div>
+          </AnimatePresence>
+      </div>
     </div>
   );
 }
