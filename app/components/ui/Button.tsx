ea25feee02b31d207d53ee99a227df82821f0d76
RohithR24 <srohithreddy.edu@gmail.com>
2024-10-11 14:34:21 -0700

    Feat: addin the Button

---
+++ app/components/ui/Button.tsx
@@ -0,0 +1,26 @@
+import { ButtonProps } from '@/types';
+import React from 'react';
+
+export default function Button({ title, styles }:ButtonProps ) {
+  return (
+    <button
+      className={`
+        bg-[#FF0000] 
+        hover:bg-[#CC0000] 
+        text-white 
+        font-bold 
+        py-3 
+        px-6 
+        ${styles}
+        rounded-full 
+        text-lg 
+        shadow-lg 
+        hover:shadow-xl 
+        transition-all 
+        duration-300
+      `}
+    >
+      {title}
+    </button>
+  );
+}
