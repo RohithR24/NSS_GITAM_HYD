e3e21879bc7566889d3a65551ed214cc51830827
RohithR45 <srohithreddy.edu@gmail.com>
2024-11-06 13:58:09 -0800

    Integrating the events to backend FB

--- app/programs/page.tsx
+++ app/programs/page.tsx
@@ -1,16 +1,28 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { EventCard, Tabs } from "@/app/components/ui/index";
 import {programs}  from '@/constants/index'
 import {events} from '@/constants/index'
-
+import { fetchEventsFB } from "@/api";
+import { EventCardProps } from "@/types";
 
 const EventsPage: React.FC = () => {
   const [activeTab, setActiveTab] = useState<string>(programs[0].id);
-  const upcomingEvents = events.filter((event) => !event.isPast);
-  const pastEvents = events.filter((event) => event.isPast);
+  const [upcomingEvents, setupcomingEvents] = useState<EventCardProps[]>([]);
+  const [pastEvents, setPastEvents] = useState<EventCardProps[]>([]);
+  // const upcomingEvents = events.filter((event) => !event.isPast);
+  // const pastEvents = events.filter((event) => event.isPast);
 
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const fetchedEvents = await fetchEventsFB();
+      setupcomingEvents(fetchedEvents);
+      setPastEvents(fetchedEvents);
+    };
+    fetchEvents();
+  }, []);
   return (
     <div className="max-w-6xl mx-auto px-4 py-8 bg-blue-50">
       <h1 className="text-4xl font-bold text-center mb-8 text-blue-900">
